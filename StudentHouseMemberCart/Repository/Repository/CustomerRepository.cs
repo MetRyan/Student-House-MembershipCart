5592fde537bf9cea1069a7224f21db20eb2545b4
MetRyan <kieterit@gmail.com>
2023-10-23 16:52:23 +0700

    json

--- StudentHouseMemberCart/Repository/Repository/CustomerRepository.cs
+++ StudentHouseMemberCart/Repository/Repository/CustomerRepository.cs
@@ -1,19 +1,23 @@
-﻿namespace Repository.Repository
+﻿using BussinenssObject.Models;
+using DAOS;
+using Repository.IRepository;
+
+namespace Repository.Repository
 {
-    public class CustomerRepository
+    public class CustomerRepository :ICustomerRepository
     {
-        public void addCustomer(Customer p) => CustomerDAO.AddCustomer(p);
+        public void addCustomer(CustomerManagement p) => CustomerDAOs.AddCustomer(p);
 
-        public void DeleteCustomer(Customer p) => CustomerDAO.DeleteCustomer(p);
+        public void DeleteCustomer(CustomerManagement p) => CustomerDAOs.DeleteCustomer(p);
 
 
-        public Customer GetCustomerbyId(int id) => CustomerDAO.GetCustomerbyId(id);
+        public CustomerManagement GetCustomerbyId(int id) => CustomerDAOs.GetCustomerbyId(id);
 
-        public List<Customer> GetCustomer() => CustomerDAO.GetCustomers();
+        public List<CustomerManagement> GetCustomer() => CustomerDAOs.GetCustomers();
 
-        public void updateCustomer(Customer p) => CustomerDAO.UpdateCustomer(p);
+        public void updateCustomer(CustomerManagement p) => CustomerDAOs.UpdateCustomer(p);
 
-        public bool CheckLogin(String email, String password) => CustomerDAO.CheckLogin(email, password);
-        public Customer GetCustomerByEmail(String email) => CustomerDAO.GetCustomerbyEmail(email);
-    }
+        public bool CheckLogin(String email, String password) => CustomerDAOs.CheckLogin(email, password);
+        public CustomerManagement GetCustomerByEmail(String email) => CustomerDAOs.GetCustomerbyEmail(email);
+    CustomerDAOs
 }
